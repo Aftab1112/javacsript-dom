02feec31774e4a519ee28f3e82ba702ed0429187
Aftab1112 <you@example.com>
2023-12-24 16:57:03 +0530

    all about dom

---
+++ script.js
@@ -0,0 +1,41 @@
+// let divs = document.querySelectorAll(".box")
+// console.log(divs)
+
+// let div = document.querySelector("#box")
+// console.dir(div)
+
+// let val = div.getAttribute("id")
+// console.log(val)
+
+// 
+
+// let div = document.querySelector("#box")
+// console.log(div)
+
+// div.style.backgroundColor = "green"
+
+// let newButton = document.createElement("button")
+// newButton.innerText = "Click Me !"
+
+// let div = document.querySelector("#box")
+// div.before(newButton)
+
+
+// let newHeading = document.createElement("h1")
+// newHeading.innerHTML = "<i>Hello</i> "
+
+// let div = document.querySelector("#box").before(newHeading)
+
+// let para = document.querySelector(".para")
+// para.remove(para)
+
+// newHeading.remove()
+
+// let btn = document.createElement("Button")
+// btn.innerText = "Click Me !"
+// btn.style.backgroundColor = "red"
+// btn.style.color = "white"
+
+// document.querySelector("body").before(btn)
+
+document.querySelector(".content").classList.add("newClass")
